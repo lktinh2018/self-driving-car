69f817730a18444545853545ba89a99aff7c491c
LKT <lktinh2018@gmail.com>
2018-09-16 18:09:47 +0700

    Update

--- Test/LoadImage.py
+++ Test/LoadImage.py
@@ -32,7 +32,7 @@ def load_train():
     print('Read train images')
     for j in range(10):
         print('Load folder c{}'.format(j))
-        path = os.path.join('..', 'input', 'imgs', 'train', 'c' + str(j), '*.jpg')
+        path = os.path.join('../train_data/1', 'input', 'imgs', 'train', 'c' + str(j), '*.jpg')
         files = glob.glob(path)
         for fl in files:
             img = get_im(fl)
@@ -141,95 +141,124 @@ def validate_holdout(model, holdout, target):
     return score
 
 
-cache_path = os.path.join('cache', 'train.dat')
-if not os.path.isfile(cache_path):
-    train_data, train_target = load_train()
-    cache_data((train_data, train_target), cache_path)
-else:
-    print('Restore train from cache!')
-    (train_data, train_target) = restore_data(cache_path)
+#cache_path = os.path.join('cache', 'train.dat')
+#if not os.path.isfile(cache_path):
+#    train_data, train_target = load_train()
+#    cache_data((train_data, train_target), cache_path)
+#else:
+#    print('Restore train from cache!')
+#    (train_data, train_target) = restore_data(cache_path)
+#
+#batch_size = 64
+#nb_classes = 10
+#nb_epoch = 2
+## input image dimensions
+#img_rows, img_cols = 96, 128
+## number of convolutional filters to use
+#nb_filters = 32
+## size of pooling area for max pooling
+#nb_pool = 2
+## convolution kernel size
+#nb_conv = 3
+#
+#train_data = np.array(train_data, dtype=np.uint8)
+#train_target = np.array(train_target, dtype=np.uint8)
+#train_data = train_data.reshape(train_data.shape[0], 1, img_rows, img_cols)
+## train_data = train_data.transpose((0, 3, 1, 2))
+#train_target = np_utils.to_categorical(train_target, nb_classes)
+#train_data = train_data.astype('float32')
+#train_data /= 255
+#print('Train shape:', train_data.shape)
+#print(train_data.shape[0], 'train samples')
+#
+#X_train, X_test, X_holdout, Y_train, Y_test, Y_holdout = split_validation_set_with_hold_out(train_data, train_target, 0.2)
+#print('Split train: ', len(X_train))
+#print('Split valid: ', len(X_test))
+#print('Split holdout: ', len(X_holdout))
+#
+#
+#model_from_cache = 0
+#if model_from_cache == 1:
+#    model = read_model()
+#    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+#else:
+#    model = Sequential()
+#    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+#                            border_mode='valid',
+#                            input_shape=(1, img_rows, img_cols)))
+#    model.add(Activation('relu'))
+#    model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+#    model.add(Activation('relu'))
+#    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+#    model.add(Dropout(0.25))
+#
+#    model.add(Flatten())
+#    model.add(Dense(128))
+#    model.add(Activation('relu'))
+#    model.add(Dropout(0.5))
+#    model.add(Dense(nb_classes))
+#    model.add(Activation('softmax'))
+#
+#    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+#
+#    '''
+#    model.fit(train_data, train_target, batch_size=batch_size, nb_epoch=nb_epoch,
+#              show_accuracy=True, verbose=1, validation_split=0.1)
+#    '''
+#    model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+#              show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
 
-batch_size = 64
-nb_classes = 10
-nb_epoch = 2
-# input image dimensions
-img_rows, img_cols = 96, 128
-# number of convolutional filters to use
-nb_filters = 32
-# size of pooling area for max pooling
-nb_pool = 2
-# convolution kernel size
-nb_conv = 3
+#score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+#print('Score: ', score)
+#score = model.evaluate(X_holdout, Y_holdout, show_accuracy=True, verbose=0)
+#print('Score holdout: ', score)
+#validate_holdout(model, X_holdout, Y_holdout)
+#save_model(model)
+#
+#cache_path = os.path.join('cache', 'test.dat')
+#if not os.path.isfile(cache_path):
+#    test_data, test_id = load_test()
+#    cache_data((test_data, test_id), cache_path)
+#else:
+#    print('Restore test from cache!')
+#    (test_data, test_id) = restore_data(cache_path)
+#
+#test_data = np.array(test_data, dtype=np.uint8)
+#test_data = test_data.reshape(test_data.shape[0], 1, img_rows, img_cols)
+## test_data = test_data.transpose((0, 3, 1, 2))
+#test_data = test_data.astype('float32')
+#test_data /= 255
+#print('Test shape:', test_data.shape)
+#print(test_data.shape[0], 'test samples')
+#predictions = model.predict(test_data, batch_size=128, verbose=1)
 
-train_data = np.array(train_data, dtype=np.uint8)
-train_target = np.array(train_target, dtype=np.uint8)
-train_data = train_data.reshape(train_data.shape[0], 1, img_rows, img_cols)
-# train_data = train_data.transpose((0, 3, 1, 2))
-train_target = np_utils.to_categorical(train_target, nb_classes)
-train_data = train_data.astype('float32')
-train_data /= 255
-print('Train shape:', train_data.shape)
-print(train_data.shape[0], 'train samples')
-
-X_train, X_test, X_holdout, Y_train, Y_test, Y_holdout = split_validation_set_with_hold_out(train_data, train_target, 0.2)
-print('Split train: ', len(X_train))
-print('Split valid: ', len(X_test))
-print('Split holdout: ', len(X_holdout))
+train_data = load_train()
+test_data  = load_test()
+print(train_data)
+print(test_data)
 
 
-model_from_cache = 0
-if model_from_cache == 1:
-    model = read_model()
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
-else:
-    model = Sequential()
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                            border_mode='valid',
-                            input_shape=(1, img_rows, img_cols)))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    model.add(Dropout(0.25))
 
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
 
-    '''
-    model.fit(train_data, train_target, batch_size=batch_size, nb_epoch=nb_epoch,
-              show_accuracy=True, verbose=1, validation_split=0.1)
-    '''
-    model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-              show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
 
-score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
-print('Score: ', score)
-score = model.evaluate(X_holdout, Y_holdout, show_accuracy=True, verbose=0)
-print('Score holdout: ', score)
-validate_holdout(model, X_holdout, Y_holdout)
-save_model(model)
 
-cache_path = os.path.join('cache', 'test.dat')
-if not os.path.isfile(cache_path):
-    test_data, test_id = load_test()
-    cache_data((test_data, test_id), cache_path)
-else:
-    print('Restore test from cache!')
-    (test_data, test_id) = restore_data(cache_path)
 
-test_data = np.array(test_data, dtype=np.uint8)
-test_data = test_data.reshape(test_data.shape[0], 1, img_rows, img_cols)
-# test_data = test_data.transpose((0, 3, 1, 2))
-test_data = test_data.astype('float32')
-test_data /= 255
-print('Test shape:', test_data.shape)
-print(test_data.shape[0], 'test samples')
-predictions = model.predict(test_data, batch_size=128, verbose=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 create_submission(predictions, test_id, score)
--- raspberry/app.py
+++ raspberry/app.py
@@ -8,14 +8,13 @@ import io
 class App(object):
 
     signal = ""
-    bayer_mode = True
-
+    done = False
+    autoMode = False
+    
     def __init__(self):
         self.initSerial()
         self.initCamera()
         self.initSocketServer()
-        print("ABC")
-
 
     def initCamera(self):
         camera = PiCamera()
@@ -31,21 +30,22 @@ class App(object):
 
     def handleCamera(self):
       stream = io.BytesIO()
-      for count, foo in enumerate(self.camera.capture_continuous( stream, format='png', bayer=self.bayer_mode)):
+      for count, foo in enumerate(self.camera.capture_continuous( stream, format='png', bayer=True)):
             # Get number of bytes in the stream
             num_of_bytes = stream.tell()
             # Rewind the stream to start
             stream.seek(0)
             # Save stream contents to file
-            if self.signal == "1" or self.signal == "3" or self.signal == "4":
+            if self.signal == "1" or self.signal == "3" or self.signal == "4" and self.done == False:
               if self.signal == "1":
-                save_path = "../train_data/1/img%d.png" % count
+                save_path = "../train_data/0/img%d.png" % count
               elif self.signal == "3":
-                save_path = "../train_data/3/img%d.png" % count
+                save_path = "../train_data/1/img%d.png" % count
               elif self.signal == "4":
-                save_path = "../train_data/4/img%d.png" % count
+                save_path = "../train_data/2/img%d.png" % count
               with open(save_path, 'wb') as f:
                 f.write(stream.read(num_of_bytes))
+              self.done = True
 
             # Empty the stream
             stream.seek(0)
@@ -80,11 +80,21 @@ class App(object):
         while True:
             data = c.recv(2).decode()
             self.signal = data[0]
+            
+            if self.signal == "8":
+              if self.autoMode:
+                print("Deactive autonomous mode")
+                self.autoMode = False
+              else:
+                print("Active autonomous mode")
+                self.autoMode = True
             #print(data)
+            
+            
             data += "\r\n"
             data = data.encode()
             self.serial.write(data)
-
+            self.done = False
             if data == "EXIT":
                 print("Close socket client")
                 break
