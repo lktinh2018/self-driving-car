1cd9c6ae0d7594cbe3547a411afa284cd30b5a02
Lâm Khả Tính <lktinh2018@gmail.com>
2018-09-23 14:18:05 +0700

    Update from PC

--- raspberry/app.py
+++ raspberry/app.py
@@ -6,6 +6,22 @@ from time import sleep
 import io 
 import os
 
+# Libs for CNN
+import keras
+from keras.datasets import mnist
+from keras import backend as K
+from keras.models import load_model
+import time
+
+# Var for CNN
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# Input image dimensions
+img_rows, img_cols = 28, 28
+IMG_ROWS, IMG_COLS = 28, 28
+
 class App(object):
 
     # Train data path
@@ -40,8 +56,36 @@ class App(object):
 
     def handleCar(self):
         print("Set up car handling successful")
+        # Load train and test sets
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        if K.image_data_format() == 'channels_first':
+            x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+            x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+            input_shape = (1, img_rows, img_cols)
+        else:
+            x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+            x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+            input_shape = (img_rows, img_cols, 1)
+        x_train = x_train.astype('float32')
+        x_test = x_test.astype('float32')
+        x_train /= 255
+        x_test /= 255
+        print('x_train shape:', x_train.shape)
+        # Convert class vectors to binary class matrices
+        y_train = keras.utils.to_categorical(y_train, num_classes)
+        y_test = keras.utils.to_categorical(y_test, num_classes)
+        # Load model
+        new_model = load_model('MNIST.h5')
+        print("Load model successful !!!")
         while True:
             if self.autoMode:
+                start = time.time()
+                new_img = x_test[0]
+                new_img = new_img.reshape((1, 28, 28, 1))
+                result = new_model.predict_classes(new_img)
+                end = time.time()
+                print("Time elapse: ", end - start)
+                print("Predict value: ", result)
                 self.signal = "3"
                 self.serial.write((self.signal + "\r\n").encode())
                 sleep(1)
