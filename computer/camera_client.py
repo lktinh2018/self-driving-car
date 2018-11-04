d7b8b174735d45419e96abc949cd8f9603eeec0d
Lâm Khả Tính <lktinh2018@gmail.com>
2018-11-04 17:47:12 +0700

    Update Camera Client

--- computer/camera_client.py
+++ computer/camera_client.py
@@ -4,6 +4,9 @@ import numpy as np
 import cv2
 
 class CameraClient(object):
+
+    streamSocket = ""
+
     def __init__(self):
         self.initSocketClient()
 
@@ -16,22 +19,23 @@ class CameraClient(object):
         print("Connected to camera server")
         try:
             print("Streaming...")
-            stream_bytes = ' '
+            stream_bytes = " "
+            
             while True:
-                stream_bytes += streamSocket.read(1024).decode()
+                stream_bytes += self.streamSocket.recv(1024).decode('utf-8')
                 first = stream_bytes.find('\xff\xd8')
-                last = stream_bytes.find('\xff\xd9')
+                last  = stream_bytes.find('\xff\xd9')
                 if first != -1 and last != -1:
                     jpg = stream_bytes[first:last + 2]
                     stream_bytes = stream_bytes[last + 2:]
                     image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_UNCHANGED)
 
-                    
+
                     #cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
                     #cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
                     cv2.imshow('Camera', image)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
         finally:
             clientSocket.close()
